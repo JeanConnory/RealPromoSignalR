cee09a2eba68c97534bfc81dfd7ed7efa44871d2
Jean Connory <michaelrhcp@gmail.com>
2021-05-18 22:00:52 -0300

    projeto com as telas criadas

---
+++ RealPromo.ApiWeb/Hubs/PromoHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealPromo.ApiWeb.Models;
+using System.Threading.Tasks;
+
+namespace RealPromo.ApiWeb.Hubs
+{
+    public class PromoHub : Hub
+    {
+        public async Task CadastrarPromocao(Promocao promocao)
+        {
+            await Clients.Caller.SendAsync("CadastradoSucesso");
+            await Clients.Others.SendAsync("ReceberPromocao", promocao);
+        }
+    }
+}
